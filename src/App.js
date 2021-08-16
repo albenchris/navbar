2b7b1a0a28a97991980bbed3f97aba5d702f528c
albenchris <albenchris00@gmail.com>
2021-08-16 15:44:20 -0500

    initial desktop styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Switch>
+          {/* <Route exact path='/' component={Home} /> */}
+        </Switch>
+      </Router>
       Hi
     </div>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,25 @@
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const NavBar = () => {
+    const subscribe = () => {
+        console.log('You have subscribed!');
+    }
+
     return (
         <nav>
             <h1>NAVIGATIOn</h1>
+
+            <ul className='nav-links'>
+                <li><Link>Our mission</Link></li>
+                <li><Link>Who we help</Link></li>
+                <li><Link>Get involved</Link></li>
+                <li><Link>Contact</Link></li>
+            </ul>
+
+            <div>
+                <button onClick={subscribe}>Subscribe</button>
+            </div>
         </nav>
     );
 }
